80e64f98f80105b22d81f3851128d728163fc935
Rodrigo <rodrigo.rivera@ciecnias.unam.mx>
2022-01-18 23:45:13 -0600

    correciones

--- js/scripts.js
+++ js/scripts.js
@@ -388,50 +388,88 @@ Portfolio
     var gridContainer = $('#grid-container-fullwidth'),
         filtersContainer = $('#filters-container-fullwidth'),
         wrap, filtersCallback;
-
     gridContainer.cubeportfolio({
-        defaultFilter: '*',
-        animationType: 'fadeOut',
+        layoutMode: 'grid',
+        rewindNav: true,
+        scrollByPage: false,
         gapHorizontal: 0,
         gapVertical: 0,
         gridAdjustment: 'responsive',
-        caption: 'zoom',
-        displayType: 'lazyLoading',
-        displayTypeSpeed: 100,
-        // lightbox
-        lightboxDelegate: '.cbp-lightbox',
-        lightboxGallery: true,
-        lightboxTitleSrc: 'data-title',
-        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
-        // singlePage popup
-        singlePageDelegate: '.cbp-singlePage',
-        singlePageDeeplinking: true,
-        singlePageStickyNavigation: true,
-        singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
-        singlePageCallback: function (url, element) {
-            // to update singlePage content use the following method: this.updateSinglePage(yourContent)
-            var t = this;
-            $.ajax({
-                url: url,
-                type: 'GET',
-                dataType: 'html',
-                timeout: 5000
-            })
-                .done(function (result) {
-                    t.updateSinglePage(result);
-                })
-                .fail(function () {
-                    t.updateSinglePage("Error! Please refresh the page!");
-                });
-        },
-        // single page inline
-        singlePageInlineDelegate: '.cbp-singlePageInline',
-        singlePageInlinePosition: 'above',
-        singlePageInlineInFocus: true,
-        singlePageInlineCallback: function (url, element) {
-            // to update singlePage Inline content use the following method: this.updateSinglePageInline(yourContent)
-        }
-    });
+        mediaQueries: [
+            {
+                width: 1100,
+                cols: 1
+            }, {
+                width: 800,
+                cols: 1
+            }, {
+                width: 500,
+                cols: 1
+            }, {
+                width: 320,
+                cols: 1
+            }
+        ]
+
+    })
+
+    // gridContainer.cubeportfolio({
+    //     defaultFilter: '*',
+    //     animationType: 'fadeOut',
+    //     gapHorizontal: 0,
+    //     gapVertical: 0,
+    //     gridAdjustment: 'responsive',
+    //     mediaQueries: [
+    //         {
+    //             width: 1100,
+    //             cols: 2
+    //         }, {
+    //             width: 800,
+    //             cols: 2
+    //         }, {
+    //             width: 500,
+    //             cols: 2
+    //         }, {
+    //             width: 320,
+    //             cols: 1
+    //         }],
+    //     caption: 'zoom',
+    //     displayType: 'lazyLoading',
+    //     displayTypeSpeed: 100,
+    //     // lightbox
+    //     lightboxDelegate: '.cbp-lightbox',
+    //     lightboxGallery: true,
+    //     lightboxTitleSrc: 'data-title',
+    //     lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} de {{total}}</div>',
+    //     // singlePage popup
+    //     singlePageDelegate: '.cbp-singlePage',
+    //     singlePageDeeplinking: true,
+    //     singlePageStickyNavigation: true,
+    //     singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} de {{total}}</div>',
+    //     singlePageCallback: function (url, element) {
+    //         // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+    //         var t = this;
+    //         $.ajax({
+    //             url: url,
+    //             type: 'GET',
+    //             dataType: 'html',
+    //             timeout: 5000
+    //         })
+    //             .done(function (result) {
+    //                 t.updateSinglePage(result);
+    //             })
+    //             .fail(function () {
+    //                 t.updateSinglePage("Error! Please refresh the page!");
+    //             });
+    //     },
+    //     // single page inline
+    //     singlePageInlineDelegate: '.cbp-singlePageInline',
+    //     singlePageInlinePosition: 'above',
+    //     singlePageInlineInFocus: true,
+    //     singlePageInlineCallback: function (url, element) {
+    //         // to update singlePage Inline content use the following method: this.updateSinglePageInline(yourContent)
+    //     }
+    // });
     /* add listener for filters */
     if (filtersContainer.hasClass('cbp-l-filters-dropdown')) {
         wrap = filtersContainer.find('.cbp-l-filters-dropdownWrap');
