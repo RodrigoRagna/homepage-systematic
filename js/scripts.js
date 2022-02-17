9a0c3222af9bf674cb712fef4a0bb5d3a3266b4a
Rodrigo <rodrigo.rivera@ciecnias.unam.mx>
2022-02-17 10:59:41 -0600

    correcciones y whats app

--- js/scripts.js
+++ js/scripts.js
@@ -202,12 +202,8 @@
         /* -------------------
         Back to top button popup
         ---------------------*/
-        if ($(window).scrollTop() > 400) {
-            $("#back-to-top").stop().animate({ bottom: '16px' }, 300, 'easeInOutCubic')
-        }
-        else {
-            $("#back-to-top").stop().animate({ bottom: '-50px' }, 300, 'easeInOutCubic')
-        }
+    
+        
     });
     /* -------------------
     Preloader
@@ -240,8 +236,10 @@
     Back to top button function
     ---------------------*/
     $('#back-to-top,.to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 1000, 'easeInOutExpo');
-        return false;
+        
+        
+
+        
     });
     /* -------------------
     Active menu item on page scroll
